0d15a6df03d01b9f2cdc5abbc12366917a2bcdbd
Diego Linares <diegojeanluck@hotmail.com>
2022-12-07 15:19:58 -0500

    Fix: pycodestyle

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
     You have n number of locked boxes in front of you.
-    Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+    Each box is numbered sequentially from 0 to n - 1
+    and each box may contain keys to the other boxes.
 """
 
 from typing import List
@@ -9,12 +10,14 @@ from typing import List
 Box = List[int]
 Boxes = List[Box]
 
+
 def canUnlockAll(boxes: Boxes) -> bool:
     """
     Compute the boxes and verify if all can be unlocked.
 
     Keyword arguments:
-    boxes: list[list[int]] each element is a box that contains natural numbers that represents a key, to open other boxes.
+    boxes: list[list[int]] each element is a box that contains
+    natural numbers that represents a key, to open other boxes.
 
     Return: True if all the boxes can be unlocked False otherwise
     """
@@ -29,11 +32,11 @@ def canUnlockAll(boxes: Boxes) -> bool:
 
     for key in keys:
         for i in range(0, len(boxes[key])):
-            actual_key = boxes[key][i]
-            if (actual_key not in keys and actual_key <= number_boxes - 1 and actual_key):
+            curr_key = boxes[key][i]
+            if (curr_key and curr_key < number_boxes and curr_key not in keys):
                 keys.append(boxes[key][i])
 
     if (len(keys) == number_boxes - 1):
-            unlocked_all = True
+        unlocked_all = True
 
     return (unlocked_all)
