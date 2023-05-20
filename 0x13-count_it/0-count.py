10deeeecdfe4617973fd9412665f9f95626aa614
Diego Linares <diegojeanluck@hotmail.com>
2023-05-20 15:38:41 -0500

    Using other way

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -5,73 +5,39 @@ the title of all hot articles, and prints a sortedcount
 of given keywords (case-insensitive, delimited by spaces).
 """
 
-import re
 from requests import get
 
 header = {
     'User-Agent': 'Linux:api_advanced:v0.0.0 (by /u/ElEnriquez)'
 }
 
+def get_data(subreddit: str, hot_list: list, after: str) -> list:
+    """Recursive function that queries the Reddit API, parses hot articles"""
+    if after is None:
+        return hot_list
+    url = "https://www.reddit.com/r/{}/hot.json?limit=100".format(subreddit)
+    res = get(url, headers=header, params={'after': after})
+    if res.status_code != 200:
+        return []
+    after = res.json().get('data').get('after')
+    data = res.json().get('data').get('children', [])
+    for i in data:
+        hot_list += (i.get('data').get('title').lower().split())
+    return get_data(subreddit, hot_list, after)
 
-def iterate_children(childrens, word_list, count_of_word):
-    """
-    Go through the childrens of response of Reddit API
-
-    Args:
-        childrens (List[dict]): List of reddits obtained from the subreddit
-        word_list (List[str]): List of wordts to search on titles
-        count_of_word (dict): Dictionary with the
-        count of the matching words on the titles
-    """
-
-    regex = r'({})'.format('|'.join(word_list))
-
-    for children in childrens:
-        title = children.get('data').get('title')
-        re_obj = re.search(regex, title, re.IGNORECASE)
-        if (re_obj is None):
-            continue
-        founded = [element.lower() for element in re_obj.groups()]
-        for word in founded:
-            if (count_of_word.get(word)):
-                count_of_word[word] += 1
-            else:
-                count_of_word[word] = 1
-
-
-def count_words(subreddit, word_list, after='start', count_of_words={}):
-    """
-    Count words on the hot topics of one subreddit.
-    and count it doing match with the word_list
-
-    Args:
-        subreddit (str): Subreddit to search the hot topics.
-        word_list (List[str]): Words to search on the subreddit.
-        after (str | None): String to go to the next page
-        of the response about subreddit.
-        count_of_word (dict): Dictionary with the count of words to search.
-
-    Return: None
-    """
-    reddit_url = 'https://www.reddit.com'
-    base_url = '{}/r/{}/hot.json?after={}#'.format(reddit_url, subreddit, after)
-    response = get(base_url, headers=header, allow_redirects=False)
-
-    if ((response.status_code != 200) or (after is None)):
-        return
-
-    data = response.json().get('data')
-    elements = data.get('children')
-
-    if (after == 'start'):
-        word_list = set([word.lower() for word in word_list])
-
-    iterate_children(elements, word_list, count_of_words)
-    count_words(subreddit, word_list, data.get('after'), count_of_words)
-
-    if (len(count_of_words) != 0 and after == 'start'):
-        count_of_words = dict(
-            sorted(count_of_words.items(),
-                   key=lambda item: (-item[1], item[0])))
-        for key, value in count_of_words.items():
-            print("{}: {}".format(key, value))
+def count_words(subreddit: str, word_list: list) -> None:
+    """Prints a sorted count of given keywords
+    (case-insensitive, delimited by spaces. Javascript should count as
+    javascript, but java should not)"""
+    new_list = [i.lower() for i in word_list]
+    hot_list = get_data(subreddit, [], "")
+    current_dict = {}
+    for i in new_list:
+        if current_dict.get(i):
+            current_dict[i] += hot_list.count(i)
+        else:
+            current_dict[i] = hot_list.count(i)
+    new = sorted(current_dict.items(), key=lambda y: y[1], reverse=True)
+    for i, j in new:
+        if j != 0:
+            print("{}: {}".format(i, j))
