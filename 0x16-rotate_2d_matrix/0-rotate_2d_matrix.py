2692e8360b00645ba194cf5e9252a20060d83e63
Diego Linares <diegojeanluck@hotmail.com>
2023-06-04 03:42:09 -0500

    Fix: pycodestyle

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,7 +9,7 @@ Rotate 2D Matrix
 def rotate_2d_matrix(matrix: list) -> None:
     """
     Given an n x n 2D matrix, rotate it 90 degrees clockwise
-    
+ 
     Args:
         matrix [list]: Matrix to be rotated
     """
