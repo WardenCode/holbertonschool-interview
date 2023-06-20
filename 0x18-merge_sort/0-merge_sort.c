063ddb380e88d6af5f08d10d02d18f6fc7947423
Diego Linares <diegojeanluck@hotmail.com>
2023-06-20 18:52:04 -0500

    Feat: 0. Merge sort

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,147 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * print_array_with_range - Print a part of the array
+ *
+ * @array: Array to print
+ *
+ * @left: Start index of array
+ *
+ * @right: End index of array
+ *
+ * Return: Nothing
+ */
+void print_array_with_range(int *array, int left, int right)
+{
+	int i = 0;
+
+	if (!array)
+		return;
+
+	for (i = left; i < right; i++)
+	{
+		if (i != left)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	putchar('\n');
+}
+
+/**
+ * copy_array - copy an array to another
+ *
+ * @src: Source array
+ *
+ * @start: Array index to start copying
+ *
+ * @end: Array index to end copying
+ *
+ * @dest: Destination array
+ *
+ * Return: Void
+ */
+void copy_array(int *src, size_t start, size_t end, int *dest)
+{
+	size_t i = start;
+
+	for (; i < end; i++)
+		dest[i] = src[i];
+}
+
+/**
+ * top_down_merge - Top down merge method
+ *
+ * @B: Secondary Array
+ *
+ * @start: Array index to start
+ *
+ * @mid: Array index to middle
+ *
+ * @end: Array index to end
+ *
+ * @A: Primary Array
+ *
+ * Return: Void
+ */
+void top_down_merge(int *B, size_t start, size_t mid, size_t end, int *A)
+{
+	size_t i = start, j = mid, k = start;
+
+	if (!A || !B)
+		return;
+
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array_with_range(A, start, mid);
+	printf("[right]: ");
+	print_array_with_range(A, mid, end);
+
+	for (; k < end; k++)
+	{
+		if (i < mid && (j >= end || A[i] <= A[j]))
+		{
+			B[k] = A[i];
+			i = i + 1;
+		}
+		else
+		{
+			B[k] = A[j];
+			j = j + 1;
+		}
+	}
+
+	printf("[Done]: ");
+	print_array_with_range(B, start, end);
+}
+
+/**
+ * top_down_split_merge -Top down split method
+ *
+ * @B: Secondary Array
+ *
+ * @start: Array index to start
+ *
+ * @end: Array index to end
+ *
+ * @A: Primary Array
+ *
+ * Return: Void
+ */
+void top_down_split_merge(int *B, size_t start, size_t end, int *A)
+{
+	size_t mid = (start + end) / 2;
+
+	if (end - start <= 1)
+		return;
+
+	top_down_split_merge(A, start, mid, B);
+	top_down_split_merge(A, mid, end, B);
+
+	top_down_merge(B, start, mid, end, A);
+}
+
+/**
+ * merge-sort - Merge and sort two arrays
+ *
+ * @array: Int pointer to the array
+ *
+ * @size: Size of the array
+ *
+ * Return: void
+ */
+void merge_sort(int *array, size_t size)
+{
+	int *array2 = NULL;
+
+	if (!array || size < 2)
+		return;
+
+	array2 = malloc(sizeof(int) * size);
+	if (!array2)
+		return;
+
+	copy_array(array, 0, size, array2);
+	top_down_split_merge(array2, 0, size, array);
+	free(array2);
+}
