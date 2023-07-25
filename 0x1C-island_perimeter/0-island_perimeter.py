6f28da8e0eeade6777a2d177dfb6ac2f06a57815
Diego Linares <diegojeanluck@hotmail.com>
2023-07-25 17:06:22 -0500

    0. Island Perimeter

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Island Perimeter
+"""
+
+
+def island_perimeter(grid: list[list[int]]) -> int:
+    """
+    Get the island perimeter in a ocean grid
+
+    Args:
+        grid (list[list[int]]): Matrix with the water an earth surface
+
+    Returns:
+        int: Perimeter of the earth surface
+    """
+    perimeter: int = 0
+
+    for y in range(0, len(grid)):
+        for x in range(0, len(grid)):
+            tmp = 4
+
+            if (grid[y][x] == 0):
+                continue
+
+            if (((x + 1) < len(grid)) and (grid[y][x + 1] == 1)):
+                tmp -= 1
+
+            if (((x - 1) >= 0) and (grid[y][x - 1] == 1)):
+                tmp -= 1
+
+            if (((y + 1) < len(grid)) and (grid[y + 1][x] == 1)):
+                tmp -= 1
+
+            if (((y - 1) >= 0) and (grid[y - 1][x] == 1)):
+                tmp -= 1
+
+            perimeter += tmp
+
+    return perimeter
